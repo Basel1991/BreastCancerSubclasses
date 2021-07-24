deab820b852859cbb306ee9a6326beb20d4733a4
Basel Alyafi <basel931991@gmail.com>
2021-07-24 15:37:34 +0200

    Update load.py

--- functions/load.py
+++ functions/load.py
@@ -5,15 +5,13 @@ def encoder(hormone1, hormone2, hormone3):
     """
     encoder(hormon1, hormon2, hormon3)
     this function takes the status of three hormones and encodes them in Binary.
-    example: hormone1 = 'RN', hormone2='RP', hormone3='RP', code=6
+    example: hormone1 = 'RN', hormone2='RP', hormone3='RP', code=3
     any 'RU' value will return false.
-
     Inputs:
     -------
     hormone1: str, hormone1 status (the most significant bit)
     hormone2: str, hormone2 status
     hormone3: str, hormone3 status
-
     Returns:
     --------
     code: int, the code representing the three hormones
@@ -29,5 +27,5 @@ def encoder(hormone1, hormone2, hormone3):
             print('Hormone undefined')
             return -1
 
-    else:
-        return np.dot(hormones == 'RP', 2 ** multiplicand)
+        else:
+            return np.dot(hormones == 'RP', 2 ** multiplicand)
